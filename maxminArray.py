491560d42e0e8b6f6576f13a60b157d775e7e2a9
Ashvinjeet-singh <ashvinjeetsingh98@gmail.com>
2020-10-21 22:59:55 +0530

    Maximum and minimum of array .PY

--- maxminArray.py
+++ maxminArray.py
@@ -1,15 +1,16 @@
 arr=[1,2,2,4,5,6,7,10,2,15,80,2,0,-20];
-max=0
-min=0
-i=0;
-j=i+1;
-end=len(arr)-1
-while(end>=0):
+print("Maximum Number",max(arr),"\nMinimum Number",min(arr))
+# can be done but not required
+# maximum=max(arr)
+# minimum=min(arr)
+# i=0;
+# j=i+1;
+# end=len(arr)-1
+# # while(end>=0):
 	
-	if(arr[i]>max):
-		max=arr[i]
-	if(arr[i]<arr[j]):
-		min=arr[i]
-	i+=1
-	end-=1
-print("Maximum Number",max,"\nMinimum Number",min)
+# # 	if(arr[i]>max):
+# # 		max=arr[i]
+# # 	if(arr[i]<arr[j]):
+# # 		min=arr[i]
+# # 	i+=1
+# # 	end-=1
